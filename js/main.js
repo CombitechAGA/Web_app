26677cc4ceef383508153f14a5e17f3c94e13e31
thomasstrahl <thomasstrahl@gmail.com>
2015-07-15 11:51:20 +0200

    started routing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var app = angular.module('app', ['dx', 'ui.router']);
+
+var app = angular.module('app', ['dx', 'ui.router'])
+
+.config(function ($stateProvider) {
+	$stateProvider
+	.state('dashboard', {
+      url: "/home",
+      templateUrl: "dashboard.html",
+      controller: 'MainController'
+
+    })
+
+})
+
+.run(['$state', function ($state) {
+   $state.transitionTo('dashboard');
+}])
